563d027e03d4d70956df0758c2c1a4345d7ae74b
jungdami <jungdamiing@gmail.com>
2024-05-07 12:13:02 +0900

    빌드 에러 처리

--- src/app/customerFeedbackSurvey/[id]/page.tsx
+++ src/app/customerFeedbackSurvey/[id]/page.tsx
@@ -3,7 +3,7 @@
 import Header from "@/components/header";
 import styles from "./page.module.scss";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
 
 export default function CustomerFeedbackSurvey({
@@ -12,55 +12,59 @@ export default function CustomerFeedbackSurvey({
   params: { id: string };
 }) {
   const router = useRouter();
-  const questions = [
-    {
-      questionId: "1",
-      questionType: "1",
-      questionTitl: "에이닥을 쓰면서<br />얼마나 만족하시나요?",
-      questionSubTitl: "",
-      answerList: [
-        { titl: "매우 만족", scr: 5 },
-        { titl: "만족", scr: 4 },
-        { titl: "보통", scr: 3 },
-        { titl: "불만족", scr: 2 },
-        { titl: "매우 불만족", scr: 1 },
-      ],
-    },
-    {
-      questionId: "2",
-      questionType: "2",
-      questionTitl: "에이닥에게<br />전하고 싶은 의견이 있나요?",
-      questionSubTitl: "적어주신 의견을 꼼꼼하게 살피고 검토할게요.",
-      answerList: [],
-      answerPlaceholder: "입력하기",
-    },
-    {
-      questionId: "3",
-      questionType: "2",
-      questionTitl:
-        "주변 사람들에게 에이닥을<br />추천하고 싶은 정도는 얼마일까요?",
-      questionSubTitl: "",
-      answerList: [],
-      answerPlaceholder: "1-10점 사이로 추천 정도를 적어주세요ex) 10점",
-    },
-    {
-      questionId: "4",
-      questionType: "2",
-      questionTitl: "에이닥 사용성 평가<br />참가자를 모집해요",
-      questionSubTitl:
-        "에이닥에 대한 의견을 공유해가며<br />저희 서비스를 같이 완성해보세요",
-      answerList: [],
-      answerPlaceholder: "이메일을 입력해 주세요.",
-    },
-  ];
+  const questions = useMemo(
+    () => [
+      {
+        questionId: "1",
+        questionType: "1",
+        questionTitl: "에이닥을 쓰면서<br />얼마나 만족하시나요?",
+        questionSubTitl: "",
+        answerList: [
+          { titl: "매우 만족", scr: 5 },
+          { titl: "만족", scr: 4 },
+          { titl: "보통", scr: 3 },
+          { titl: "불만족", scr: 2 },
+          { titl: "매우 불만족", scr: 1 },
+        ],
+      },
+      {
+        questionId: "2",
+        questionType: "2",
+        questionTitl: "에이닥에게<br />전하고 싶은 의견이 있나요?",
+        questionSubTitl: "적어주신 의견을 꼼꼼하게 살피고 검토할게요.",
+        answerList: [],
+        answerPlaceholder: "입력하기",
+      },
+      {
+        questionId: "3",
+        questionType: "2",
+        questionTitl:
+          "주변 사람들에게 에이닥을<br />추천하고 싶은 정도는 얼마일까요?",
+        questionSubTitl: "",
+        answerList: [],
+        answerPlaceholder: "1-10점 사이로 추천 정도를 적어주세요\nex) 10점",
+      },
+      {
+        questionId: "4",
+        questionType: "2",
+        questionTitl: "에이닥 사용성 평가<br />참가자를 모집해요",
+        questionSubTitl:
+          "에이닥에 대한 의견을 공유해가며<br />저희 서비스를 같이 완성해보세요",
+        answerList: [],
+        answerPlaceholder: "이메일을 입력해 주세요.",
+      },
+    ],
+    []
+  );
   console.log("id", params.id);
-
   const [qst, setQst] = useState(null);
   const [current, setCurrent] = useState(+params.id);
   const qstInfo = questions.find((q) => q.questionId === params.id);
+
   useEffect(() => {
     setQst(qstInfo);
-  }, []);
+  }, [qstInfo]);
+
   console.log("qstInfo", qstInfo);
   console.log("qst", qst);
 
